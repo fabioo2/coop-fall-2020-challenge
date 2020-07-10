be3bbd01a01a635dce77c00b5d10829cdacb931a
fabioo2 <fabio.kim2@gmail.com>
2020-07-10 14:51:21 -0400

    final

--- solution_javascript.js
+++ solution_javascript.js
@@ -1,14 +1,49 @@
 class EventSourcer {
-  constructor() {
-    this.value = 0;
-  }
+    constructor() {
+        this.value = 0;
+        this.storage = 0;
+        this.array = [];
+    }
 
-  add(num) {}
-  subtract(num) {}
-  undo() {}
-  redo() {}
-  bulk_undo(num) {}
-  bulk_redo(num) {}
+    add(num) {
+        this.array.push(num);
+        this.value = this.array.reduce((a, b) => a + b, 0);
+        console.log(this.array);
+        console.log(this.value);
+    }
+    subtract(num) {
+        this.array.push(-num);
+        this.value = this.array.reduce((a, b) => a + b, 0);
+        console.log(this.array);
+        console.log(this.value);
+    }
+    undo() {
+        this.storage = this.array[this.array.length - 1];
+        this.array.pop();
+        this.value = this.array.reduce((a, b) => a + b, 0);
+        console.log(this.array);
+        console.log(this.value);
+    }
+    redo() {
+        this.array.push(this.storage);
+        this.value = this.array.reduce((a, b) => a + b, 0);
+        console.log(this.array);
+        console.log(this.value);
+    }
+    bulk_undo(num) {
+        for (let i = 0; i < num; i++) {
+            this.undo();
+        }
+        console.log(this.array);
+        console.log(this.value);
+    }
+    bulk_redo(num) {
+        for (let i = 0; i < num; i++) {
+            this.redo();
+        }
+        console.log(this.array);
+        console.log(this.value);
+    }
 }
 
 // ----- Do not modify anything below this line (needed for test suite) ------
